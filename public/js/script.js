155ddb03f79fdd0fa8e0d4b2de21f85373f0048b
Francisco J. Riveira <frj.riveira@gmail.com>
2019-10-18 13:06:44 +0200

    Part 3 finished, posting and SHOWING comments

--- public/js/script.js
+++ public/js/script.js
@@ -1,96 +1,112 @@
 (function() {
-    Vue.component("first-component", {
-        template: "#template",
-        data: function() {
-            return {
-                images: [],
-                username: "",
-                desc: "",
-                title: "",
-                url: "",
-                comments: [],
-                created_at: "",
-                usercmt: "",
-                comment: ""
-            };
-        },
-        props: ["postTitle", "selectedPic"],
-        mounted: function() {
-            var myVue = this;
-            axios.get(`/images/${this.selectedPic}`).then(function(resp) {
-                myVue.images = resp.data[0];
-            });
-        },
-        methods: {
-            closeModal: function() {
-                console.log("emitting from the component...");
-                this.$emit("close", true);
-            },
-            submitComment: function() {
-                console.log("Submit comment runs");
-                var myVue = this;
-                let commentContent = {
-                    currImg: this.selectedPic,
-                    usercmt: this.usercmt,
-                    comment: this.comment
-                };
-                axios.post("/comment", commentContent).then(function(resp) {
-                    console.log("response for comments", resp);
-                    myVue.comments.unshift(commentContent);
-                });
-            }
-        }
-    });
+	Vue.component('first-component', {
+		template: '#template',
+		data: function() {
+			return {
+				images: [],
+				username: '',
+				desc: '',
+				title: '',
+				url: '',
+				comments: [],
+				created_at: '',
+				usercmt: '',
+				comment: ''
+			};
+		},
+		props: [ 'postTitle', 'selectedPic' ],
+		mounted: function() {
+			var myVue = this;
+			axios
+				.get(`/images/${this.selectedPic}`)
+				.then(function(resp) {
+					myVue.images = resp.data[0];
+				})
+				.catch(function(e) {
+					console.log(e);
+				});
 
-    new Vue({
-        el: "#main",
-        data: {
-            images: [],
-            username: "",
-            desc: "",
-            title: "",
-            file: null,
-            selectedPic: null
-        },
-        created: function() {
-            console.log("created");
-        },
-        mounted: function() {
-            console.log("mounted");
-            var myVue = this;
-            axios.get("/images").then(function(resp) {
-                myVue.images = resp.data;
-            });
-        },
-        updated: function() {
-            console.log("updated");
-        },
-        methods: {
-            closeMe: function() {
-                console.log("Closing thingies, CloseMe running");
-                this.selectedPic = null;
-            },
-            upload: function() {
-                var myVue = this;
-                var fd = new FormData();
-                fd.append("image", this.file);
-                fd.append("username", this.username);
-                fd.append("title", this.title);
-                fd.append("desc", this.desc);
-                axios.post("/upload", fd).then(function(res) {
-                    myVue.images.unshift(res.data);
-                    myVue.username = res.data.username;
-                    myVue.title = res.data.title;
-                    myVue.desc = res.data.desc;
-                });
-            },
-            fileSelected: function(e) {
-                this.file = e.target.files[0];
-            },
-            imageClicked: function(id) {
-                // .0console.log(id); // This works, it shows the image id we clicked in.
-                this.selectedPic = id;
-            }
-        }
-    });
+			axios
+				.get(`/comment?id=${this.selectedPic}`)
+				.then(
+					function(resp) {
+						myVue.comments = resp.data;
+					}.bind(myVue)
+				)
+				.catch(function(err) {
+					console.log('This is an error fetching the comments', err);
+				});
+		},
+		methods: {
+			closeModal: function() {
+				console.log('emitting from the component...');
+				this.$emit('close', true);
+			},
+			submitComment: function() {
+				console.log('Submit comment runs');
+				var myVue = this;
+				let commentContent = {
+					currImg: this.selectedPic,
+					usercmt: this.usercmt,
+					comment: this.comment
+				};
+				axios.post('/comment', commentContent).then(function(resp) {
+					console.log('response for comments', resp);
+					myVue.comments.unshift(commentContent);
+				});
+			}
+		}
+	});
+
+	new Vue({
+		el: '#main',
+		data: {
+			images: [],
+			username: '',
+			desc: '',
+			title: '',
+			file: null,
+			selectedPic: null
+		},
+		created: function() {
+			console.log('created');
+		},
+		mounted: function() {
+			console.log('mounted');
+			var myVue = this;
+			axios.get('/images').then(function(resp) {
+				myVue.images = resp.data;
+			});
+		},
+		updated: function() {
+			console.log('updated');
+		},
+		methods: {
+			closeMe: function() {
+				console.log('Closing thingies, CloseMe running');
+				this.selectedPic = null;
+			},
+			upload: function() {
+				var myVue = this;
+				var fd = new FormData();
+				fd.append('image', this.file);
+				fd.append('username', this.username);
+				fd.append('title', this.title);
+				fd.append('desc', this.desc);
+				axios.post('/upload', fd).then(function(res) {
+					myVue.images.unshift(res.data);
+					myVue.username = res.data.username;
+					myVue.title = res.data.title;
+					myVue.desc = res.data.desc;
+				});
+			},
+			fileSelected: function(e) {
+				this.file = e.target.files[0];
+			},
+			imageClicked: function(id) {
+				// .0console.log(id); // This works, it shows the image id we clicked in.
+				this.selectedPic = id;
+			}
+		}
+	});
 })();
