d4b7d5ceeee2e7d3a5112eed2f69b783b11fce96
Francisco J. Riveira <frj.riveira@gmail.com>
2019-10-20 22:24:09 +0200

    Modified a DB Query, finished the styling and the sample pictures for Mondays presentation

--- db.js
+++ db.js
@@ -1,41 +1,41 @@
-const spicedPg = require("spiced-pg");
-const db = spicedPg(
-    process.env.DATABASE_URL ||
-        `postgres:postgres:postgres@localhost:5432/imageboard`
-);
+const spicedPg = require('spiced-pg');
+const db = spicedPg(process.env.DATABASE_URL || `postgres:postgres:postgres@localhost:5432/imageboard`);
 
 module.exports.getPictures = () => {
-    return db.query(`SELECT * FROM images ORDER BY created_at DESC LIMIT 15;`);
+	return db.query(`SELECT * FROM images ORDER BY created_at DESC LIMIT 12;`);
 };
 
 module.exports.addPictures = (username, title, desc, imageUrl) => {
-    return db.query(
-        `INSERT INTO images (username, title, description, url) values ($1, $2, $3, $4) RETURNING id;`,
-        [username, title, desc, imageUrl]
-    );
+	return db.query(`INSERT INTO images (username, title, description, url) values ($1, $2, $3, $4) RETURNING id;`, [
+		username,
+		title,
+		desc,
+		imageUrl
+	]);
 };
 
-module.exports.getPicInfo = id => {
-    return db.query(`SELECT * FROM images WHERE id = $1;`, [id]);
+module.exports.getPicInfo = (id) => {
+	return db.query(`SELECT * FROM images WHERE id = $1;`, [ id ]);
 };
 
 module.exports.postingComments = (usercmt, comment, currImg) => {
-    return db.query(
-        `INSERT INTO comments (comment, usercmt, imageId) values ($1, $2, $3) RETURNING id`,
-        [comment, usercmt, currImg]
-    );
+	return db.query(`INSERT INTO comments (comment, usercmt, imageId) values ($1, $2, $3) RETURNING id`, [
+		comment,
+		usercmt,
+		currImg
+	]);
 };
 
-module.exports.showAllComments = imageId => {
-    return db.query(`SELECT * FROM comments WHERE imageId = $1`, [imageId]);
+module.exports.showAllComments = (imageId) => {
+	return db.query(`SELECT * FROM comments WHERE imageId = $1 ORDER BY created_at DESC`, [ imageId ]);
 };
 
-module.exports.getMoreImages = lastId => {
-    return db.query(
-        `SELECT * FROM images
+module.exports.getMoreImages = (lastId) => {
+	return db.query(
+		`SELECT * FROM images
         WHERE id < $1
         ORDER BY id DESC
-        LIMIT 10`,
-        [lastId]
-    );
+        LIMIT 4`,
+		[ lastId ]
+	);
 };
